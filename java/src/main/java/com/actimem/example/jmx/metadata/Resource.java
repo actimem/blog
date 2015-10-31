5cd814e96d9ec45b484e942a24d71e0093ef3cb5
Eduard <eduardmanas@yahoo.com>
2015-10-31 14:04:39 +0000

    Updated Copyright

--- java/src/main/java/com/actimem/example/jmx/metadata/Resource.java
+++ java/src/main/java/com/actimem/example/jmx/metadata/Resource.java
@@ -1,13 +1,13 @@
 /*
- * Copyright 2012 The Apache Software Foundation
+ * Copyright 2015 BCN IT Consulting Ltd
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -15,10 +15,10 @@
  */
 package com.actimem.example.jmx.metadata;
 
+import com.actimem.example.jmx.mxbean.ResourceItem;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import com.actimem.example.jmx.mxbean.ResourceItem;
 
 public class Resource implements ResourceMXBean {
 	List<String> items = new ArrayList<String>();
--- projects/jaxb/src/main/java/com/actimem/blog/jaxb/schemagen/package-info.java
+++ projects/jaxb/src/main/java/com/actimem/blog/jaxb/schemagen/package-info.java
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 @XmlSchema(namespace = "http://actimem.com/blog/jaxb/company", elementFormDefault = XmlNsForm.QUALIFIED)
